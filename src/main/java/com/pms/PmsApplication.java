9087c60a048def55a069976b8aff111fde9bfdc4
“mhatreapurva” <amhatre@syr.edu>
2022-04-23 15:09:34 -0400

    All the unnecessary modules have been removed!

--- src/main/java/com/pms/PmsApplication.java
+++ src/main/java/com/pms/PmsApplication.java
@@ -1,31 +1,19 @@
 package com.pms;
 
-import com.pms.model.Role;
-import com.pms.model.Student;
-import com.pms.model.User;
-import com.pms.repository.StudentRepository;
-import com.pms.repository.UserRepository;
-import com.pms.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 @ComponentScan("com.pms")
 @EnableMongoRepositories
 public class PmsApplication {
-	@Autowired
-	StudentRepository studentRepository;
+
 	public static void main(String[] args) {
 
 		SpringApplication.run(PmsApplication.class, args);
